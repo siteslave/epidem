ebd05f6a4b32e90dabad573b5d5da08420ec4de9
kataynoi <thait-rex@hotmail.com>
2013-05-30 13:58:13 +0700

    ทดสอบ Push
    ทดสอบ Push

---
+++ application/controllers/reports.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Created by JetBrains PhpStorm.
+ * User: spiderman
+ * Date: 30/5/2556
+ * Time: 13:52 น.
+ * To change this template use File | Settings | File Templates.
+ */
+class Reports extends CI_Controller {
+
+    /**
+     * Index controller
+     */
+    public function index()
+    {
+        $this->layout->view('reports/index_view');
+    }
+
+}
---
+++ application/views/reports/index_view.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: spiderman
+ * Date: 30/5/2556
+ * Time: 13:53 น.
+ * To change this template use File | Settings | File Templates.
+ */
+echo "Main Reports";
